bf60cbfec78e8d42c52821da58cbeb582fbe71f7
Darío Valenzuela <daro.valenzuelaa@gmail.com>
2026-04-20 10:16:33 -0400

    Update poo_gato.py

--- Python/poo_gato.py
+++ Python/poo_gato.py
@@ -1,1 +1,8 @@
+class Gato:
+    color="default"
+    def hablar(self):
+        print(f"Miau, soy un gato que solo chinga y además soy {self.color}")
 
+Salem=Gato()
+Salem.color="negro"
+Salem.hablar()
