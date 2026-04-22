4dffda7c4d11cd4289c942ec16e140c0405fdf0c
Darío Valenzuela <daro.valenzuelaa@gmail.com>
2026-04-21 20:25:53 -0400

    Update personaje.py

--- Python/personaje.py
+++ Python/personaje.py
@@ -1,1 +1,47 @@
+```python```
 
+class Personaje:
+
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
+
+    def atributos(self):
+        print(self.nombre, ":", sep="")
+        print("·Fuerza:", self.fuerza)
+        print("·Inteligencia:", self.inteligencia)
+        print("·Defensa:", self.defensa)
+        print("·Vida:", self.vida)
+
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza = self.fuerza + fuerza
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa = self.defensa + defensa
+
+    def esta_vivo(self):
+        return self.vida > 0
+    
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+
+    def daño(self, enemigo):
+        return self.fuerza - enemigo.defensa
+    
+    def atacar(self, enemigo):
+        daño = self.daño(enemigo)
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        if enemigo.esta_vivo():
+            print("La vida de", enemigo.nombre, "es", enemigo.vida)
+        else:
+            enemigo.morir()
+
+            
+mi_personaje = Personaje("Undertaker", 10, 1, 5, 100)
+mi_personaje.atributos()
+mi_personaje.subir_nivel(1, 2, 0)
+mi_personaje.atributos()
