7f3fc0c0d2ec19cb7a0fdfa45569ca50789c3edd
Darío Valenzuela <daro.valenzuelaa@gmail.com>
2026-05-08 00:08:03 -0400

    Add files via upload

---
+++ Java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       Lenguaje html = new Lenguaje("HTML", 1993);
+       html.descripción();
+
+       Lenguaje css = new Lenguaje("CSS", 1996);
+       css.descripción();
+
+       Lenguaje javascript = new Lenguaje("JavaScript", 1995);
+       javascript.descripción();
+    }
+}
